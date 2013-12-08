c35f84e6aebc82487945b2b32f1d4ebc749683e9
Stephen Blum <blum.stephen@gmail.com>
2013-12-07 20:56:52 -0800

    enhanced animations and added last trend arrow.

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,10 +7,17 @@ var btc_current = updater('btc-current', false )
 
 // MTGOX TICKER DISPLAY UPDATES
 PUBNUB.events.bind( 'ticker.BTCUSD', function(data) {
+    // SET LAST ARROW CHANGE UP/DOWN
+    var up   = 'glyphicon glyphicon-chevron-up icon-green'
+    ,   down = 'glyphicon glyphicon-chevron-down icon-red';
+
+    PUBNUB.$('btc-current-arrow').className = 
+        +data.ticker.last.value > btc_current.last ? up : down;
+
     // SET CURRENT LOW AND HIGH VALUES
-    btc_current(data.ticker.last.display_short);
-    btc_high(   data.ticker.high.display_short);
-    btc_low(    data.ticker.low.display_short);
+    btc_current(data.ticker.last);
+    btc_high(   data.ticker.high);
+    btc_low(    data.ticker.low);
 } );
 
 // MTGOX TRADES (BUY/SELL)
@@ -22,17 +29,21 @@ PUBNUB.events.bind( 'ticker.BTCUSD', function(data) {
 function updater( name, noanimate ) {
     var node = PUBNUB.$(name);
 
-    function fn(value) {
-        var up   = { d          : 1,
-                     ty         : -10,
-                     background : 'rgba(92,184,92,0.8)',
-                     color      : '#fff' };
-        var down = { d          : 1,
-                     ty         : 10,
-                     background : 'rgba(217, 83, 79,0.8)',
-                     color      : '#fff' };
+    function fn(msg) {
+        var display = msg.display_short
+        ,   value   = +msg.value
+        ,   up      = {
+            d          : 1,
+            ty         : -10,
+            background : 'rgba( 92, 184, 92, 0.8 )',
+            color      : '#fff' }
+        ,   down    = {
+            d          : 1,
+            ty         : 10,
+            background : 'rgba( 217, 83, 79, 0.8 )',
+            color      : '#fff' };
 
-        node.innerHTML = value;
+        node.innerHTML = display;
         (fn.last != value) && !noanimate && animate( node, [
             (fn.last < value) ? up : down,
             { d          : 1,
